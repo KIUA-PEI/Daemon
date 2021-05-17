24c3b304b153e45d311d448441a84d910adba76c
Pedro Abreu <pedro.abreu@ua.pt>
2021-05-17 20:41:14 +0100

    update rogueAP

--- metrics/rogueAP.py
+++ metrics/rogueAP.py
@@ -16,10 +16,16 @@ def num_rogue_ap_data(token, week_ago=None):
         week_ago = datetime.now() - timedelta(days=7)
 
     r = requests.get("https://wso2-gw.ua.pt/primecore_primecore-ws/1.0.0/RogueAccessPointAlarm/Count", headers={'Authorization': token})
+
+    print("r.status_code:" +  str(r.status_code))
     
     if r.status_code == 401:
         token = get_acess_token()
         return num_rogue_ap_data(token, week_ago)
+
+    if r.status_code == 400:
+        
+        return None
     
     elif r.status_code == 200:
         num_entrys = json.loads(r.text)["count"]
@@ -36,10 +42,14 @@ def num_rogue_ap_data(token, week_ago=None):
                 token = get_acess_token()
                 return num_rogue_ap_data(token, week_ago)
 
+            if r.status_code == 500:
+                print("internal server error!")
+
             if r.status_code == 200:
                 data = json.loads(r.text)
                 for rogue in data["rogueAccessPointAlarms"]:
                     time = dt.datetime.strptime(rogue["timeStamp"], '%Y-%m-%dT%H:%M:%S.%fZ')
+                    print(time.isoformat())
                     print("time > week_ago: "+str(time > week_ago))
                     if time > week_ago:
                         num_rogue_ap = num_rogue_ap + 1 if rogue["severity"] == "MINOR" or rogue["severity"] == "CRITICAL" else num_rogue_ap
